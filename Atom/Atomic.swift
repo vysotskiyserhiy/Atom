8235be762a6a50a7fe26e6eb7521dd53284ce5b6
Serge Vysotsky <vysotskiyserhiy@gmail.com>
2018-11-11 11:28:59 +0200

    0.1.3

--- Atom/Atomic.swift
+++ Atom/Atomic.swift
@@ -20,10 +20,6 @@ public class Atomic<A> {
         return queue.sync(flags: .barrier) { _value }
     }
 
-    public func map<M>(_ transform: (A) throws -> M) -> M? {
-        return queue.sync { try? transform(_value) }
-    }
-
     @discardableResult
     public func mutate<M>(_ transform: (inout A) throws -> M) rethrows -> M {
         return try queue.sync(flags: .barrier) { try transform(&_value) }
